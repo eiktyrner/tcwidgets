589a32084f4966e3833b8e9e88927c10c781f55f
Ian W <ian.uu12@gmail.com>
2016-03-15 19:43:59 -0700

    Update for Thaumcraft 5

--- tree/main.js
+++ tree/main.js
@@ -1,37 +1,42 @@
 var input = null;
 var result = null;
 $(document).ready(function(){
-	input = $("#aspect");
-	result = $("#result");
+	input = $('#aspect');
+	result = $('#result');
 	
-	input.on("keydown", function(event){
-		if(event.keyCode == 13) show($(this).val().toLowerCase());
+	input.on('keydown', function(event){
+		if(event.keyCode == 13) show();
 	});
 });
 function show(asp){
-	result.attr("class", "").empty();//Clear everything
-	$("table > tbody > tr:not(#input-row)").remove();
+	var asp = input.val().toLowerCase();
+	result.attr('class', '').empty();//Clear everything
+	$('table > tbody > tr:not(#input-row)').remove();
 	var label = asp.slice(0, 1).toUpperCase() + asp.slice(1);
-	var aspect = aspects[asp];
+	var aspect = aspects.selected[asp];
 	if(aspect){
-		result.text("Showing component and user trees of ");
-		result.append($("<img src='" + aspect.iconURL + "'/>").css("vertical-align", "middle"));
-		result.append($("<b>" + label + "</b>"));
+		result.text('Showing component and user trees of ');
+		result.append($("<img src='" + aspect.iconURL + "'/>").css('vertical-align', 'middle'));
+		result.append($('<b>' + label + '</b>'));
 		
-		$("#components-wrapper > ul, #users-wrapper > ul").empty();
+		$('#components-wrapper > ul, #users-wrapper > ul').empty();
 		
-		render(aspect, $("#components-wrapper > ul"), "components");
-		render(aspect, $("#users-wrapper > ul"), "users");
+		render(aspect, $('#components-wrapper > ul'), 'components');
+		render(aspect, $('#users-wrapper > ul'), 'users');
 	}else{
-		result.addClass("error").text("Unknown aspect '" + label + "'");
+		result.addClass('error').text("Unknown aspect '" + label + "'");
 	}
 }
 function render(aspect, parent, fn){
-	var li = $("<li></li>").appendTo(parent);
-	var text = $("<p></p>").appendTo(li);
-	text.css("background-image", "url(" + aspect.iconURL + ")");
+	var li = $('<li></li>').appendTo(parent);
+	var text = $('<p></p>').appendTo(li);
+	text.css('background-image', 'url(' + aspect.iconURL + ')');
 	text.append("<span class='aspect-name'>" + aspect.displayName + "</span>").append("<span class='aspect-description'>" + aspect.displayDescription+ "</span>");
-	var ul = $("<ul></ul>").appendTo(li);
+	var ul = $('<ul></ul>').appendTo(li);
 	var array = aspect[fn];
 	for(var asp in array) render(array[asp], ul, fn);
+}
+function updateVersion(){
+	$('#components-wrapper > ul > *, #users-wrapper > ul > *').remove();
+	show();
 }
